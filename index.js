93dc6339ff5b020505d714fd9853d98805126f55
travis skyles <travis.skyles@gmail.com>
2020-01-15 23:16:14 -0800

    added to index

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 'use strict';
 
 const server = require('./lib/server');
+const mongoose = require('mongoose');
 
+const mongooseOptions = {
+  useNewUrlParser:true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+};
+mongoose.connect(process.env.MONGODB_URI, mongooseOptions);
 server.start();
