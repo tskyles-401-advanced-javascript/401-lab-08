f6533c0d9022c9ee4b757c80f9a8369e39d14f29
travis skyles <travis.skyles@gmail.com>
2020-01-15 22:55:14 -0800

    finished product routes

---
+++ routes/products-routes.js
@@ -0,0 +1,53 @@
+'use strict';
+
+const express = require('express');
+const router = express.Router();
+const products = require('../models/products-model');
+
+router.get('/api/v1/products', getProducts);
+router.get('/api/v1/products/:id', getProduct);
+router.post('/api/v1/products/:id', postProduct);
+router.put('/api/v1/products/:id', putProduct);
+router.delete('/api/v1/products/:id', deleteProduct);
+/**
+ * function for get routes
+ * @param {*} req
+ * @param {*} res
+ * @param {*} next
+ */
+function getProducts(req, res, next){
+  products.get()
+    .then(data => {
+      const output = {
+        count: data.length,
+        results: data,
+      };
+      res.status(200).json(output);
+    })
+    .catch(next);
+}
+
+function getProduct(req, res, next){
+  products.get(req.params.id)
+    .then(results => res.status(200).json(results[0]))
+    .catch(next);
+}
+
+function postProduct(req, res, next){
+  products.post(req.body)
+    .then(results => res.status(200).json(results[0]))
+    .catch(next);
+}
+
+function putProduct(req, res, next){
+  products.put(req.params.id, req.body)
+    .then(results => res.status(200).json(results[0]))
+    .catch(next);
+}
+
+function deleteProduct(req, res, next){
+  products.delete(req.params.id)
+    .then(results => res.status(200).json(results));
+}
+
+module.exports = router;
