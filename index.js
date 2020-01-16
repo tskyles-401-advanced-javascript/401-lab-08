df49a5b9a18e3fcbd4d1dd37341e8d01147f6984
travis skyles <travis.skyles@gmail.com>
2020-01-15 23:20:59 -0800

    server up and running

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 'use strict';
 
+require('dotenv').config();
 const server = require('./lib/server');
 const mongoose = require('mongoose');
 
--- routes/categories-routes.js
+++ routes/categories-routes.js
@@ -2,7 +2,7 @@
 
 const express = require('express');
 const router = express.Router();
-const Categories = require('../models/category-model');
+const Categories = require('../models/categories-model');
 
 const categories = new Categories;
 
