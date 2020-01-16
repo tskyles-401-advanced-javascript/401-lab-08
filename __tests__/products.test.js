86ff5cab3a492fca2671144dd4487eb6f01f5cbe
travis skyles <travis.skyles@gmail.com>
2020-01-15 22:39:23 -0800

    created post test

--- __tests__/products.test.js
+++ __tests__/products.test.js
@@ -10,18 +10,48 @@ describe('product routes', () => {
       .then(record => {
         Object.keys(testProduct).forEach(key => {
           expect(record[key]).toEqual(testProduct[key]);
-        })
-      })
-  })
+        });
+      });
+  });
 
-  it('should get a new food', () => {
-    return product.get()
-      .then(record => {
-        Object.keys(testProduct).forEach(key => {
-          expect(record[key]).toEqual(testProduct[key]);
-        })
-      })
-  })
+  it('should get() products', () => {
+    const obj = { name: 'test', quantity: 10 };
+    return mockRequest.post('/api/v1/products')
+      .send(obj)
+      .then(results => {
+        return mockRequest.get('/api/v1/products')
+          .then(data => {
+            Object.keys(testProduct).forEach(key => {
+              expect(data.body.results[0][key]).toEqual(obj[key]);
+            });
+          });
+      });
+  });
+
+  it('should get() a product', () => {
+    const obj = { name: 'test', quantity: 10 };
+    return mockRequest.post('/api/v1/products')
+      .send(obj)
+      .then(results => {
+        return mockRequest.get(`/api/v1/products/${results.body.id}`)
+          .then(data => {
+            Object.keys(obj).forEach(key => {
+              expect(data.body[key]).toEqual(obj[key]);
+            });
+          });
+      });
+  });
+
+  it('should post a product', () => {
+    const obj = { name: 'test', quantity: 10 };
+    return mockRequest.post('/api/v1/products')
+      .send(obj)
+      .then(results => {
+        Object.keys(obj).forEach(key => {
+          expect(results.body[key]).toEqual(obj[key]);
+        });
+      });
+  });
 
   it('should update a new food', () => {
     let updated = { name: 'carrots', price: 1 };
@@ -30,7 +60,7 @@ describe('product routes', () => {
       .then(record => {
         Object.keys(testProduct).forEach(key => {
           expect(record[key]).toEqual(testProduct[key]);
-        })
-      })
-  })
-} 
+        });
+      });
+  });
+});
